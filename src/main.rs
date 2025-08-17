6aab9110434685319a3fae1641820bdaeaad441e
Pablo G Alvarez <pabloga9@hotmail.com>
2025-08-17 14:09:22 +0200

    Move html to a new folder

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     if request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
+        let contents = fs::read_to_string("html/hello.html").unwrap();
         let length = contents.len();
 
         let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
@@ -27,7 +27,7 @@ fn handle_connection(mut stream: TcpStream) {
         stream.write_all(response.as_bytes()).unwrap();
     } else {
         let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
+        let contents = fs::read_to_string("html/404.html").unwrap();
         let length = contents.len();
 
         let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
